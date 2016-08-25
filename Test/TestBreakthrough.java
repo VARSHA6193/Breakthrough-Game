234ce6928bec69a0729ca1f934eeb00594527693
VARSHA6193 <varshakvn@gmail.com>
2016-08-25 12:25:16 -0400

    Update TestBreakthrough.java

--- Test/TestBreakthrough.java
+++ Test/TestBreakthrough.java
@@ -5,17 +5,6 @@ import org.junit.Test;
 //import BreakthroughImpl.PieceType;
 //import BreakthroughImpl.PlayerType;
 import org.junit.*;
-/**
- * Initial test case class for Breakthrough
- * 
- * This source code is from the book "Flexible, Reliable Software: Using
- * Patterns and Agile Development" published 2010 by CRC Press. Author: Henrik B
- * Christensen Computer Science Department Aarhus University
- * 
- * This source code is provided WITHOUT ANY WARRANTY either expressed or
- * implied. You may study, use, modify, and distribute it for non-commercial
- * purposes. For any commercial use, see http://www.baerbak.com/
- */
 public class TestBreakthrough {
 	Breakthrough game;
 
